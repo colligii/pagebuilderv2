0d12fdf6dfb17ecb7507711868a55a697994668d
Giovanne Colli <cgiovanne45@gmail.com>
2025-11-06 12:04:15 -0300

    fix class error

--- dist/components/util/normalizeKeyProps.js
+++ dist/components/util/normalizeKeyProps.js
@@ -1,5 +1,15 @@
 export default function normalizeKeyProps(key, ctx, props = {}, css = {}) {
-    props.class += (props.class ?? '') + ' ' + Object.entries(css).map(([key, value]) => ctx?.style.registerCss(key, value)).join(' ');
+    let className = [];
+    const cssEntries = Object.entries(css);
+    if (props.class)
+        className.push(props.class);
+    if (cssEntries?.length)
+        className = [...className,
+            cssEntries.map(([key, value]) => ctx?.style.registerCss(key, value)).join(' ')
+        ];
+    if (className?.length) {
+        props.class = className.join(" ");
+    }
     const keyWithProps = [
         key,
         ...(Object.entries(props).map(([key, value]) => `${key}="${value}"`))
--- src/components/util/normalizeKeyProps.ts
+++ src/components/util/normalizeKeyProps.ts
@@ -1,8 +1,21 @@
 import type { Context } from "./context.js"
 
 export default function normalizeKeyProps(key: string, ctx?: Context, props: { [p: string]: string } = {}, css: { [p: string]: string } = {}) {
-    props.class += (props.class ?? '') + ' ' +  Object.entries(css).map(([key, value]) => ctx?.style.registerCss(key, value)).join(' ')
+    let className = [];
+    const cssEntries = Object.entries(css);
     
+    if(props.class)
+        className.push(props.class)
+
+    if(cssEntries?.length)
+        className = [...className, 
+        cssEntries.map(([key, value]) => ctx?.style.registerCss(key, value)).join(' ')
+    ]
+
+    if(className?.length) {
+        props.class = className.join(" ")
+    }
+
     const keyWithProps = [
         key,
         ...(Object.entries(props).map(([key, value]) => `${key}="${value}"`))
