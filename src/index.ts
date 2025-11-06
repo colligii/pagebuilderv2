72d9aa04b73e28159d1be8858385f9b3b1682f8c
Giovanne Colli <cgiovanne45@gmail.com>
2025-11-06 09:49:42 -0300

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import closedComponent from "./components/closedComponent.js";
 
-console.log(closedComponent({
-    key: 'div',
-    props: {
-        'id': 'teste'
-    },
-    componentsOrText: [
-        'teste',
-        'teste',
-        'teste',
-    ]
-}))
+export default closedComponent;
